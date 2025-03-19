1602cc4819ec6e4d489bb1e3dbda35c1a6d4b1c3
syhanjin <2819469337@qq.com>
2025-03-19 14:24:51 +0800

    feat: add mobile responsive layout for tabbar

--- src/components/tabbar.tsx
+++ src/components/tabbar.tsx
@@ -4,8 +4,8 @@ interface tabbarProps {
 
 export default function Tabbar(props: tabbarProps) {
   return (
-    <div className="w-screen min-h-18 max-h-18 flex sticky z-10 shadow-2xs justify-between items-center px-24 top-0 bg-white/90">
-      <div className="z-20 flex gap-2 text-black">
+    <div className="w-screen min-h-18 max-h-18 flex flex-col sticky z-10 shadow-2xs justify-center md:justify-between items-stretch md:items-center px-2 md:flex-row md:px-24 top-0 bg-white/90">
+      <div className="z-20 flex items-center gap-2 text-black pt-4 pb-1 md:p-0">
         <img src="/src/assets/logo.webp" alt="HOA" className="w-6 h-6" />
         <a
           className="font-bold text-md hover:text-black"
@@ -14,7 +14,7 @@ export default function Tabbar(props: tabbarProps) {
           HITSZ 自动化课程攻略共享计划
         </a>
       </div>
-      <div className="font-bold text-md mr-16">{props.fileTitle}</div>
+      <div className="font-bold text-xs md:text-md md:mr-16 text-right">{props.fileTitle}</div>
     </div>
   );
 }
