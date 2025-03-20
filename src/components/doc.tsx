ab455da87eec4e7393b0af9e8441230025e21e05
longlin10086 <gnulonglin@gmail.com>
2025-03-20 11:05:44 +0800

    style: add dark mode

--- src/components/doc.tsx
+++ src/components/doc.tsx
@@ -1,11 +1,14 @@
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import { useContext } from "react";
 import "@cyntler/react-doc-viewer/dist/index.css";
+import { ThemeContext } from "../contexts/themeContext";
 
 interface docProps {
   fileURL: string;
 }
 
 export default function DocComponent(props: docProps) {
+  const { dark } = useContext(ThemeContext);
   return (
     <DocViewer
       documents={[{ uri: props.fileURL }]}
@@ -15,6 +18,7 @@ export default function DocComponent(props: docProps) {
           disableHeader: true,
         },
       }}
+      style={{ backgroundColor: dark ? "oklch(0.129 0.042 264.695)" : "white" }}
       className="mt-18"
     />
   );
